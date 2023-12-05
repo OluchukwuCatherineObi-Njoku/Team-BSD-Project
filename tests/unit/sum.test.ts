3ad1b05d9462addc4bb8a187f92c66aefcdf4cb9
Majd Al Mnayer <majd.mounayer@gmail.com>
2023-12-05 00:15:24 -0500

    updated jest test

--- tests/unit/sum.test.ts
+++ tests/unit/sum.test.ts
@@ -1,5 +1,5 @@
 // Import whatever it is that you're testing
-import sum from '../../src/sum';
+import sum from 'src/sum';
 
 // Must import jest functions every time you want to use them
 import { expect, test, describe } from '@jest/globals';
